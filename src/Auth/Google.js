6fbd5483d0a4a00a5d4a15eb9d6b85180250b1c7
nguyentrungtinh <trungtinhn300@gmail.com>
2024-10-30 09:56:56 +0700

    Update login google basic btn login href

--- src/Auth/Google.js
+++ src/Auth/Google.js
@@ -1,16 +1,21 @@
 // src/Login.js
-import React from "react";
+import React, { useEffect } from "react";
 import Url from "../Config/Url";
+import { useLocation } from "react-router-dom";
 
 const Google = () => {
-  const handleLogin = () => {
-    window.open(`${Url}/oauth2/authorization/google`, "_self");
-  };
+  const location = useLocation();
+  useEffect(() => {
+    const queryParams = new URLSearchParams(location.search);
+    const email = queryParams.get("email");
+
+    localStorage.setItem("email", email);
+  }, [location]);
 
   return (
     <div>
       <h1>Login with Google</h1>
-      <button onClick={handleLogin}>Login</button>
+      <a href={`${Url}/oauth2/authorization/google`}>Login with Google</a>
     </div>
   );
 };
